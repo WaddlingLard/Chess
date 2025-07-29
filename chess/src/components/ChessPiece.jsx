50ebf3cb7ebcdef8c8a122d6a115a3910962d99b
Brian <dathamor@gmail.com>
2025-07-29 00:59:04 -0600

    we have chess icon library!

--- chess/src/components/ChessPiece.jsx
+++ chess/src/components/ChessPiece.jsx
@@ -1,35 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
+// Importing icon libraries
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+    faChessPawn,
+    faChessKnight,
+    faChessBishop,
+    faChessRook,
+    faChessQueen,
+    faChessKing,
+} from "@fortawesome/free-solid-svg-icons";
+
+// prettier-ignore
 export const PIECE_TYPE = Object.freeze({
-    PAWN: 'PAWN',
-    KNIGHT: 'KNIGHT',
-    BISHOP: 'BISHOP',
-    ROOK: 'ROOK',
-    QUEEN: 'QUEEN',
-    KING: 'KING',
-})
+    PAWN:   { name: "PAWN",   icon: faChessPawn },
+    KNIGHT: { name: "KNIGHT", icon: faChessKnight },
+    BISHOP: { name: "BISHOP", icon: faChessBishop  },
+    ROOK:   { name: "ROOK",   icon: faChessRook },
+    QUEEN:  { name: "QUEEN",  icon: faChessQueen },
+    KING:   { name: "KING",   icon: faChessKing },
+});
 
 function ChessPiece({ name }) {
-
     const [pieceType, setPieceType] = useState(undefined);
 
     // Set the piece type
     useEffect(() => {
         if (PIECE_TYPE[name] == undefined) {
-            throw new Error('Invalid piece type provided to chess piece constructor!');
+            throw new Error(
+                "Invalid piece type provided to chess piece constructor!"
+            );
         }
 
         setPieceType(PIECE_TYPE[name]);
-    }, [])
+    }, []);
 
     return (
         <>
-            <div>
-                <p style={{ color: '#55F' }}> {pieceType} </p>
+            <div
+                style={{ display: "flex", width: "100%", height: "100%" }}
+                draggable={true}
+            >
+                {pieceType !== undefined && (
+                    <FontAwesomeIcon
+                        style={{
+                            width: "inherit",
+                            height: "80%",
+                            alignSelf: "center",
+                        }}
+                        color="#888"
+                        icon={pieceType.icon}
+                    />
+                )}
             </div>
         </>
     );
-
 }
 
 export default ChessPiece;
