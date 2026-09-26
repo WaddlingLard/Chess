2b2b3cf7315872f01ab0643e708eb24d9a3fc90b
Brian <dathamor@gmail.com>
2026-09-26 14:40:12 -0600

    applied more fields to the PIECE_MOVE_SYSTEM and handle retrieval logic here

--- chess/src/components/ChessPiece.jsx
+++ chess/src/components/ChessPiece.jsx
@@ -19,16 +19,18 @@ import { STEP_FUNCTIONS } from "../types/piece_moves";
 /**
  * @typedef {Object} MoveSystem
  * @property {StepList} steps
- * @property {number} limit
+ * @property {number} moveLimit
+ * @property {StepList} [capture]
+ * @property {number} [captureLimit]
  * @property {Record<string, Function>} conditions
  */
 
 /**
  * @typedef {{ 
- * name: string, 
+ * name: ChessPieceType, 
  * icon: any, 
  * team?: TeamType 
- * moveCount: number
+ * moveCount?: number
  * }} PieceInformation
  */
 
@@ -64,34 +66,76 @@ const invertMovesUponTeamType = (stepTuple, thisTeam, teamCondition) => {
 }
 
 /**
+ * With the reference of the piece information provided, apply the conditions to that object with the
+ * conditions provided
+ * @param {PieceInformation & { moveSystem: MoveSystem } } pieceInfo 
+ * @param {Record<string, Function>} conditions 
+ * @returns {None}
+ */
+export const applyConditions = (pieceInfo, conditions) => {
+    for (const key in conditions) {
+        const bindContextCallable = conditions[key].bind(pieceInfo)
+        pieceInfo.moveSystem[key] = bindContextCallable();
+    }
+}
+
+/**
  * How the system works:
- * Pieces will have a MOVE_SYSTEM that contains the steps, limit, and conditions
+ * Pieces will have a MOVE_SYSTEM that contains the steps, moveLimit, and conditions
  * While most pieces will follow their initial provided data, others will require a check beforehand (*eyes pawn*)
  * This is where conditions come in, they compute during runtime to recalculate the pieces initial data
  * Following this system, one could create any chess piece they desire. As long as their behavior can be tracked
- * via the step functions (steps), move path size (limit), and capture conditions (capture)
+ * via the step functions (steps), move path size (moveLimit), and capture conditions (capture)
  */
 /**@type {Record<ChessPieceType, MoveSystem} */
 export const PIECE_MOVE_SYSTEM = Object.freeze({
-    PAWN:   { steps: [...STEP_FUNCTIONS.FORWARD],                                  limit: 1, 
+    PAWN:   { steps: [...STEP_FUNCTIONS.FORWARD],                                  moveLimit: 1, 
         conditions: { 
-        steps() { return PIECE_MOVE_SYSTEM.PAWN.steps.map((stepTuple) => invertMovesUponTeamType(stepTuple, this.team, "WHITE"))}, 
-        limit() { return (this.moveCount ?? -1) == 0 ? 2 : 1; },
-        capture() { return STEP_FUNCTIONS.LEFT_AND_RIGHT.map((stepTuple) => invertMovesUponTeamType(stepTuple, this.team, "WHITE"))} 
-    } },
-    KNIGHT: { steps: [...STEP_FUNCTIONS.HORSE],                                    limit: 1, conditions: {} },
-    BISHOP: { steps: [...STEP_FUNCTIONS.DIAGONAL],                                 limit: 0, conditions: {} },
-    ROOK:   { steps: [...STEP_FUNCTIONS.INTERSECTION],                             limit: 0, conditions: {} },
-    QUEEN:  { steps: [...STEP_FUNCTIONS.DIAGONAL, ...STEP_FUNCTIONS.INTERSECTION], limit: 0, conditions: {} },
-    KING:   { steps: [...STEP_FUNCTIONS.DIAGONAL, ...STEP_FUNCTIONS.INTERSECTION], limit: 1, conditions: {} },
-    BLANK:  { steps: [], limit: 0, conditions: {} },
+            steps() { return PIECE_MOVE_SYSTEM.PAWN.steps.map((stepTuple) => invertMovesUponTeamType(stepTuple, this.team, "WHITE"))}, 
+            moveLimit() { return (this.moveCount ?? -1) == 0 ? 2 : 1; },
+            capture() { return STEP_FUNCTIONS.LEFT_AND_RIGHT.map((stepTuple) => invertMovesUponTeamType(stepTuple, this.team, "WHITE"))},
+            captureLimit() { return 1; }
+        } 
+    },
+    // PAWN:   { steps: [...STEP_FUNCTIONS.FORWARD],                                  moveLimit: 1, 
+    //     conditions: { 
+    //     steps: PIECE_MOVE_SYSTEM.PAWN.steps.map((stepTuple) => invertMovesUponTeamType(stepTuple, this.team, "WHITE")), 
+    //     moveLimit: (this.moveCount ?? -1) == 0 ? 2 : 1,
+    //     capture: STEP_FUNCTIONS.LEFT_AND_RIGHT.map((stepTuple) => invertMovesUponTeamType(stepTuple, this.team, "WHITE")) 
+    // },
+    KNIGHT: { steps: [...STEP_FUNCTIONS.HORSE],                                    moveLimit: 1, conditions: {} },
+    BISHOP: { steps: [...STEP_FUNCTIONS.DIAGONAL],                                 moveLimit: 0, conditions: {} },
+    ROOK:   { steps: [...STEP_FUNCTIONS.INTERSECTION],                             moveLimit: 0, conditions: {} },
+    QUEEN:  { steps: [...STEP_FUNCTIONS.DIAGONAL, ...STEP_FUNCTIONS.INTERSECTION], moveLimit: 0, conditions: {} },
+    KING:   { steps: [...STEP_FUNCTIONS.DIAGONAL, ...STEP_FUNCTIONS.INTERSECTION], moveLimit: 1, conditions: {} },
+    BLANK:  { steps: [], moveLimit: 0, conditions: {} },
 });
 
 const VALID_TEAM_TYPES = ["WHITE", "BLACK"];
 
+/**
+ * Gets the move system for the provided piece name
+ * @param {ChessPieceType} pieceName
+ * @param {React.Dispatch<SetStateAction<boolean>>} errorSetter 
+ * @param {React.Dispatch<SetStateAction<string>>} errorMessageSetter 
+ * @returns {MoveSystem}
+ */
+export const getMoveSystem = (pieceName, errorSetter, errorMessageSetter) => {
+
+    if (pieceName in PIECE_MOVE_SYSTEM){
+        return { ...PIECE_MOVE_SYSTEM[pieceName]}
+    } else {
+        errorSetter(true);
+        errorMessageSetter(`Failed to acquire move system for ${pieceName} chess piece!`); 
+        return PIECE_MOVE_SYSTEM.BLANK; // Default to avoid null return
+    }
+
+}
+
 function ChessPiece({
     name,
     teamType,
+    location,
     // originFromPieceSelector = { isOriginated: false, newPieceList: null },
     // drop = { validDropOccurred: false, toggleDrop: null },
 }) {
